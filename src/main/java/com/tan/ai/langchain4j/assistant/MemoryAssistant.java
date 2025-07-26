0bb8c35e9f28a063ed24995ac13a6a534e447578
tanjezh <tan19972100@163.com>
2025-07-26 16:45:07 +0800

    用户提示词的用法

--- src/main/java/com/tan/ai/langchain4j/assistant/MemoryAssistant.java
+++ src/main/java/com/tan/ai/langchain4j/assistant/MemoryAssistant.java
@@ -1,5 +1,6 @@
 package com.tan.ai.langchain4j.assistant;
 
+import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.spring.AiService;
 
 import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
@@ -11,6 +12,8 @@ import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
 @AiService(wiringMode = EXPLICIT, chatModel = "qwenChatModel", chatMemory = "chatMemory")
 public interface MemoryAssistant {
 
+    // {{it}}表示这里唯一的参数的占位符 prompt
+    @UserMessage("你是我的好朋友，请用上海话回答问题，并且添加一些表情符号。 {{it}}")
     String chat(String prompt);
 
 }
--- src/test/java/com/tan/ai/langchain4j/PromptTest.java
+++ src/test/java/com/tan/ai/langchain4j/PromptTest.java
@@ -1,5 +1,6 @@
 package com.tan.ai.langchain4j;
 
+import com.tan.ai.langchain4j.assistant.MemoryAssistant;
 import com.tan.ai.langchain4j.assistant.SeparateAssistant;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +16,25 @@ public class PromptTest {
     @Autowired
     private SeparateAssistant separateAssistant;
 
+    @Autowired
+    private MemoryAssistant memoryAssistant;
+
     @Test
     public void test(){
         String chat = separateAssistant.chat(5, "今天几号？");
         System.out.println(chat);
+    }
+
+    @Test
+    public void testMemory(){
+        String chat1 = memoryAssistant.chat( "我是瑞森");
+        System.out.println(chat1);
+
+        String chat2 = memoryAssistant.chat( "今天几号？");
+        System.out.println(chat2);
+
+        String chat3 = memoryAssistant.chat( "我是谁？");
+        System.out.println(chat3);
     }
 
 }
