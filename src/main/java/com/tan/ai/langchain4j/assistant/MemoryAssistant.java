070db79a38cdf55ec79874bfbcaf04da392d1287
tanjezh <tan19972100@163.com>
2025-07-26 16:48:39 +0800

    指定参数名称 @V 的用法

--- src/main/java/com/tan/ai/langchain4j/assistant/MemoryAssistant.java
+++ src/main/java/com/tan/ai/langchain4j/assistant/MemoryAssistant.java
@@ -1,6 +1,7 @@
 package com.tan.ai.langchain4j.assistant;
 
 import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 
 import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
@@ -13,7 +14,7 @@ import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
 public interface MemoryAssistant {
 
     // {{it}}表示这里唯一的参数的占位符 prompt
-    @UserMessage("你是我的好朋友，请用上海话回答问题，并且添加一些表情符号。 {{it}}")
-    String chat(String prompt);
+    @UserMessage("你是我的好朋友，请用上海话回答问题，并且添加一些表情符号。 {{m}}")
+    String chat(@V("m") String prompt);
 
 }
